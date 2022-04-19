4724f4c1bdaaf71760c79a227784333250401919
Shanshan Bradford <87511458+minirookie@users.noreply.github.com>
2022-04-19 11:04:12 -0500

    Update and rename autoDIMPanalyze.sh to Step2.1_PipelineCommands.sh

---
+++ Step2.1_PipelineCommands.sh
@@ -0,0 +1,25 @@
+#Fetch available online data (models) with Phenix
+for PDB in `cat PDBs.list`
+do 
+	phenix.fetch_pdb $PDB
+done
+
+
+#Run Dimple pipeline
+for PDB in `cat PDBs.list`
+do 
+	dimple $PDB.pdb /mnt/auto/home/data_directory/Bari_Jak2.mtz DIMP_$PDB
+done
+
+
+#Run Dimple analysis
+for PDB in `cat PDBs.list`
+do 
+	echo $PDB
+	grep "MTZ " DIMP_$PDB/screen.log
+	grep ".pdb"  DIMP_$PDB/screen.log
+	grep " 1 rwcontents" DIMP_$PDB/screen.log
+	grep "refmac5 restr" DIMP_$PDB/screen.log
+	echo "-------------"
+done
+
