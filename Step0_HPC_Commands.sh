931ad82a3d94f4620404eb3ac7d6b3a7bf79acd2
Shanshan Bradford <87511458+minirookie@users.noreply.github.com>
2022-04-19 10:32:07 -0500

    Update Step0_HPC_Commands.sh

--- Step0_HPC_Commands.sh
+++ Step0_HPC_Commands.sh
@@ -6,3 +6,7 @@ bkill 97768087    # to delete a job from queue use job ID
 bsub -R "rusage[mem=8000]" -n 5 -R "span[hosts=1]" -P Jamie012-09_Xia2 "/research/home/data_directory/xia2_Jamie_012-09_cmd_P3121.sh"
 bsub -R "rusage[mem=8000]" -n 5 -R "span[hosts=1]" -P Ruxo006-01_Xia2 "/research/home/data_directory/xia2_Ruxo_006-01_P3121.sh"
 bsub -R "rusage[mem=8000]" -n 5 -R "span[hosts=1]" -P Baricit_002-01_Xia2 "/research/home/data_directory/xia2_Baricit_002-01_cmd_P3121.sh"
+
+# commands to submit jobs running Phenix
+bsub -R "rusage[mem=8000]" -n 5 -R "span[hosts=1]" -P Baricit_002-01_MR "/research/home/data_directory/phenixphaser-MR-Ense.sh"
+bsub -R "rusage[mem=16000]" -n 5 -R "span[hosts=1]" -P phenix_MR_loop "/research/home/data_directory/phenixphaser-loop-MR-Ense.sh"
