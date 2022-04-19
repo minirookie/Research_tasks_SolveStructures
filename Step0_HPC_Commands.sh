f0e480e0b838fb853a168e0659ff3b7951606a84
Shanshan Bradford <87511458+minirookie@users.noreply.github.com>
2022-04-19 10:34:38 -0500

    Update Step0_HPC_Commands.sh

--- Step0_HPC_Commands.sh
+++ Step0_HPC_Commands.sh
@@ -10,3 +10,7 @@ bsub -R "rusage[mem=8000]" -n 5 -R "span[hosts=1]" -P Baricit_002-01_Xia2 "/rese
 # commands to submit jobs running Phenix
 bsub -R "rusage[mem=8000]" -n 5 -R "span[hosts=1]" -P Baricit_002-01_MR "/research/home/data_directory/phenixphaser-MR-Ense.sh"
 bsub -R "rusage[mem=16000]" -n 5 -R "span[hosts=1]" -P phenix_MR_loop "/research/home/data_directory/phenixphaser-loop-MR-Ense.sh"
+
+# commands to submit jobs running Phaser
+bsub -R "rusage[mem=20000]" -n 1 -R "span[hosts=1]" -P Ruxo_006-01_MR "/research/home/data_directory/hpc_phenixphaser_Ruxo006-01_Ense.sh"
+bsub -R "rusage[mem=16000]" -n 5 -R "span[hosts=1]" -P phenix_MR_loop "/research/home/data_directory/phenixphaser-loop-MR-Ense.sh"
