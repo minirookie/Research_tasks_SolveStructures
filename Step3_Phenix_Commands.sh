6ac0625f5b33eb7fc60c087976fa009fe45fd065
Shanshan Bradford <87511458+minirookie@users.noreply.github.com>
2022-04-19 10:41:13 -0500

    Update Step3_Phenix_Commands.sh

--- Step3_Phenix_Commands.sh
+++ Step3_Phenix_Commands.sh
@@ -4,6 +4,7 @@ source /opt/ccp4/ccp4-7.0/bin/ccp4.setup-sh
 phenix.refine /mnt/auto/data_diectory/MFP013_03-13_MRmodelEdit_Ver3.pdb \
 input.sequence.file_name="/mnt/auto/data_diectory/hTRAP1CTDInfo/MFP013.fasta" \
 input.xray_data.labels='/mnt/auto/data_diectory/MFP013IADPsoaking_02-04_2LoopsMonomerMR_2nmol.mtz:F(+),SIGF(+),F(-),SIGF(-),merge' \
+
 xray_data.force_anomalous_flag_to_be_equal_to=true \
 refinement.input.xray_data.convert_to_non_anomalous_if_ratio_pairs_lone_less_than_threshold=0.2 \
 output.serial=1 \
@@ -11,11 +12,13 @@ output.prefix=IADP013_02-04_RefineInitRun01 \
 xray_data.r_free_flags.generate=true \
 xray_data.r_free_flags.fraction=0.05 \
 xray_data.r_free_flags.disable_suitability_test=false \
+
 refinement.main.number_of_macro_cycles=2 nproc=6 \
 optimize_mask=true \
 write_eff_file=true write_geo_file=true write_model_cif_file=true \
 write_def_file=true write_maps=false write_map_coefficients=true \
 anom_map_type=llg \
+
 refinement.refine.strategy=individual_sites+individual_sites_real_space+individual_adp+occupancies+tls \
 adp.individual.anisotropic="not water and not element H" \
 optimize_xyz_weight=true optimize_adp_weight=true \
